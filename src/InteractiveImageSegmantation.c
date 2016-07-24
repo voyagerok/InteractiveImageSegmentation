1f72eee031f3394b62e2e551b9fb576f744f9776
voyagerok <shepard1010@gmail.com>
2016-07-24 14:35:31 +0300

    Set up project tree

---
+++ src/InteractiveImageSegmantation.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : InteractiveImageSegmantation.c
+ Author      : voyagerok
+ Version     :
+ Copyright   : 
+ Description : Project main file
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello"); /* prints  */
+	return EXIT_SUCCESS;
+}
